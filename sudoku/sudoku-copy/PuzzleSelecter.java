10ac9ac8ca5ea917d7e26bdbc5cb2b1cbfd1c49f
Kevin Shen <110064644+kevinhshen@users.noreply.github.com>
2026-06-01 13:25:35 -0400

    Added PuzzleSelecter

---
+++ sudoku/sudoku-copy/PuzzleSelecter.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PuzzleSelecter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PuzzleSelecter extends Actor
+{
+    /**
+     * Act - do whatever the PuzzleSelecter wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public PuzzleSelecter
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
