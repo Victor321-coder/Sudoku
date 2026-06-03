1a8a2d51c862f3e95aa18df4583627ebaa38e10c
BrendanLauSchool <349035204@gapps.yrdsb.ca>
2026-06-03 12:53:58 -0400

    made the restart button

---
+++ sudoku/sudoku-copy/Restart.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Restart here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Restart extends Actor
+{
+    public Restart () {
+        GreenfootImage img = new GreenfootImage("restart button.png"); 
+        setImage(img); 
+    }
+    /**
+     * Act - do whatever the Restart wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (Greenfoot.mouseClicked(this)) {
+            ((MyWorld)getWorld()).restart(); 
+        }
+    }
+}
