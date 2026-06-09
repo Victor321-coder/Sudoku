caf546e0ae70aee35ba00836a9a1a142c3556032
Victor321-coder <440004873@gapps.yrdsb.ca>
2026-06-09 12:47:45 -0400

    score added

--- sudoku/sudoku-copy/MyWorld.java
+++ sudoku/sudoku-copy/MyWorld.java
@@ -18,6 +18,10 @@ public class MyWorld extends World
     private long startTime;
     private boolean timerRunning;
 
+    // SCORE SYSTEM
+    private int score = 0;
+    private long lastMoveTime = System.currentTimeMillis();
+
     public MyWorld()
     {
         super(1000, 700, 1);
@@ -35,14 +39,14 @@ public class MyWorld extends World
 
         Border border = new Border(size);
 
-        int borderX = startX - size / 2 + border.getImage().getWidth() / 2-1;
-        int borderY = startY - size / 2 + border.getImage().getHeight() / 2-1;
-
+        int borderX = startX - size / 2 + border.getImage().getWidth() / 2 - 1;
+        int borderY = startY - size / 2 + border.getImage().getHeight() / 2 - 1;
 
         addObject(border, borderX, borderY);
+
         CellOutline cellOutline = new CellOutline(size);
-        addObject(cellOutline,borderX,borderY);
-        
+        addObject(cellOutline, borderX, borderY);
+
         addObject(new Restart(), 675, 150);
         addObject(new EraserButton(), 846, 158);
         addObject(new PencilButton(), 948, 158);
@@ -63,7 +67,10 @@ public class MyWorld extends World
         }
 
         updateTimer();
+        showText("Score: " + score, 850, 120);
     }
+
+    // ---------------- BOARD ----------------
 
     private void createBoard()
     {
@@ -80,11 +87,10 @@ public class MyWorld extends World
                     c,
                     size
                 );
-                
+
                 cell.setColor(new Color(168,168,168));
                 board[r][c] = cell;
 
-                
                 addObject(
                     cell,
                     startX + c * size,
@@ -106,18 +112,16 @@ public class MyWorld extends World
                     750 + c * 100,
                     250 + r * 100
                 );
-
                 num++;
             }
         }
     }
 
+    // ---------------- INPUT ----------------
+
     public void checkCellSelection()
     {
-        if(mouse == null)
-        {
-            return;
-        }
+        if(mouse == null) return;
 
         int boardX = startX - size / 2;
         int boardY = startY - size / 2;
@@ -133,18 +137,16 @@ public class MyWorld extends World
 
         int col = (mouseX - boardX) / size;
         int row = (mouseY - boardY) / size;
-        if(!board[row][col].isFixed()){
+
+        if(!board[row][col].isFixed())
+        {
             selectCell(board[row][col]);
         }
-        
     }
 
     public void checkNumberSelection()
     {
-        if(mouse == null || selectedCell == null)
-        {
-            return;
-        }
+        if(mouse == null || selectedCell == null) return;
 
         Actor clicked = mouse.getActor();
 
@@ -158,16 +160,85 @@ public class MyWorld extends World
             int correctValue =
                 solution[selectedCell.getRow()][selectedCell.getCol()];
 
+            // SPEED SYSTEM
+            long now = System.currentTimeMillis();
+            long timeDiff = now - lastMoveTime;
+            lastMoveTime = now;
+
             if(value == correctValue)
             {
                 selectedCell.setWrong(false);
+
+                int basePoints = 10;
+
+                if(timeDiff < 2000) basePoints += 10;
+                else if(timeDiff < 5000) basePoints += 5;
+
+                addScore(basePoints);
+
+                int row = selectedCell.getRow();
+                int col = selectedCell.getCol();
+
+                checkRowComplete(row);
+                checkColumnComplete(col);
+                checkGridComplete(row, col);
             }
             else
             {
                 selectedCell.setWrong(true);
+                addScore(-5);
             }
         }
     }
+
+    // ---------------- SCORE SYSTEM ----------------
+
+    private void addScore(int amount)
+    {
+        score += amount;
+        if(score < 0) score = 0;
+    }
+
+    private void checkRowComplete(int row)
+    {
+        for(int c = 0; c < 9; c++)
+        {
+            if(board[row][c].getValue() == 0)
+                return;
+        }
+
+        addScore(50);
+    }
+
+    private void checkColumnComplete(int col)
+    {
+        for(int r = 0; r < 9; r++)
+        {
+            if(board[r][col].getValue() == 0)
+                return;
+        }
+
+        addScore(50);
+    }
+
+    private void checkGridComplete(int row, int col)
+    {
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for(int r = startRow; r < startRow + 3; r++)
+        {
+            for(int c = startCol; c < startCol + 3; c++)
+            {
+                if(board[r][c].getValue() == 0)
+                    return;
+            }
+        }
+
+        addScore(50);
+    }
+
+    // ---------------- SELECTION ----------------
 
     public static void selectCell(Cell cell)
     {
@@ -184,12 +255,11 @@ public class MyWorld extends World
         }
     }
 
+    // ---------------- TIMER ----------------
+
     private void updateTimer()
     {
-        if(!timerRunning)
-        {
-            return;
-        }
+        if(!timerRunning) return;
 
         long elapsedSeconds =
             (System.currentTimeMillis() - startTime) / 1000;
@@ -213,72 +283,24 @@ public class MyWorld extends World
         timerRunning = true;
     }
 
-
-
-    // public static boolean isValidMove(Cell cell, int number)
-    // {
-        // int row = cell.getRow();
-        // int col = cell.getCol();
-
-        // // Row check
-        // for(int c = 0; c < 9; c++)
-        // {
-            // if(board[row][c] != cell &&
-               // board[row][c].getValue() == number)
-            // {
-                // return false;
-            // }
-        // }
-
-        // // Column check
-        // for(int r = 0; r < 9; r++)
-        // {
-            // if(board[r][col] != cell &&
-               // board[r][col].getValue() == number)
-            // {
-                // return false;
-            // }
-        // }
-
-        // // Box check
-        // int startRow = (row / 3) * 3;
-        // int startCol = (col / 3) * 3;
-
-        // for(int r = startRow; r < startRow + 3; r++)
-        // {
-            // for(int c = startCol; c < startCol + 3; c++)
-            // {
-                // if(board[r][c] != cell &&
-                   // board[r][c].getValue() == number)
-                // {
-                    // return false;
-                // }
-            // }
-        // }
-
-        // return true;
-    // }
-
+    // ---------------- RESTART ----------------
 
     public void restart()
     {
         resetTimer();
+        score = 0;
 
         removeObjects(getObjects(Cell.class));
-
         selectedCell = null;
 
         createBoard();
     }
 
+    // ---------------- MODES ----------------
+
     public static void setPencilMode(boolean state)
     {
         pencilMode = state;
-    }
-
-    public static boolean getPencilMode()
-    {
-        return pencilMode;
     }
 
     public static boolean isPencilMode()
