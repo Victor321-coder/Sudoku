c7d265b55dc5b51cdee84b2cb049adc805f0282c
BrendanLauSchool <349035204@gapps.yrdsb.ca>
2026-06-14 16:54:10 -0400

    some music

--- sudoku/sudoku-copy/UI.java
+++ sudoku/sudoku-copy/UI.java
@@ -2,5 +2,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class UI extends Actor
 {
-    
+    public void act(){
+        if(Greenfoot.mouseClicked(this)){
+
+            Greenfoot.playSound("clicksoundeffect.wav");
+
+        }
+    }
 }
